2da75bbeee30fcfc6836e868e8eba50430034f2d
VictorM <victormanuel.vm678@gmail.com>
2025-02-28 19:18:58 -0600

    02-función-agregar-amigos

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigo = [];
-console.log(amigo);
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    if (!nombreAmigo) {
+        alert("Debes ingresar un nombre");
+        return;
+    }
+    amigo.push(nombreAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    //console.log(amigo);
+}
 
 
